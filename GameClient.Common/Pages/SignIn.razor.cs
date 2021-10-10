046efccbd4cf464cc8fab8ba07c4ae04a1f74afa
Serhii <Sergsteamlol@gmail.com>
2021-10-10 15:22:41 +0300

    update(sesions and cookies)

--- GameClient.Common/Pages/SignIn.razor.cs
+++ GameClient.Common/Pages/SignIn.razor.cs
@@ -4,6 +4,7 @@ using GameClient.Common.Shared;
 using Microsoft.AspNetCore.Components;
 using System.Threading.Tasks;
 using Blazored.SessionStorage;
+using Microsoft.JSInterop;
 
 namespace GameClient.Common.Pages
 {
@@ -15,26 +16,42 @@ namespace GameClient.Common.Pages
         [Inject] private IApiService _apiService { get; set; }
         [Inject] private NavigationManager _uriHelper { get; set; }
         [Inject] private ISessionStorageService _sessionStorageService { get; set; }
-        private bool waitingResponse { get; set; }
-        private bool showErrorMess { get; set; } = false;
+        [Inject] private IJSRuntime _jSRuntime { get; set; }
+        private bool _waitingResponse { get; set; }
+        private bool _showErrorMess { get; set; } = false;
         public LoginModelDto LoginData { get; set; } = new();
         private bool _rememberMe { get; set; } = false;
         private LoginResponseDto _loginRequest { get; set; } = new();
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                LoginData.Login = await _jSRuntime.InvokeAsync<string>("blazorExtensions.GetCookie", "login");
+                LoginData.Password = await _jSRuntime.InvokeAsync<string>("blazorExtensions.GetCookie", "password");
+                StateHasChanged();
+            }
+        }
+
         public async Task SigninRequest()
         {
-            waitingResponse = true;
+            _waitingResponse = true;
             StateHasChanged();
             _loginRequest = (await _apiService.ExecuteRequest(() => _apiService.ApiMethods.Login(LoginData))).Data;
-            waitingResponse = false;
+            _waitingResponse = false;
 
             if (!_loginRequest.UserFind)
                 RedirectToRegistration();
 
             if (!_loginRequest.UserCanIn)
-                showErrorMess = true;
+                _showErrorMess = true;
+
+           
 
             if (_loginRequest.UserFind && _loginRequest.UserCanIn)
             {
+                if (_rememberMe)
+                    await _jSRuntime.InvokeAsync<string>("blazorExtensions.WriteCookie", LoginData.Login, LoginData.Password);
+
                 StartSession();
                 RedirectToHub();
             }
