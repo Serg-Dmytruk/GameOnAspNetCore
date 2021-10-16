5c0707bedf51e4891f92fb83f2b27e5aae1062e6
Serhii <Sergsteamlol@gmail.com>
2021-10-16 12:27:57 +0300

    update

--- GameClient.Common/Pages/CheckersDesk.razor.cs
+++ GameClient.Common/Pages/CheckersDesk.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Game.Common.ModelsDto;
 using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GameClient.Common.Pages
 {
@@ -9,8 +10,7 @@ namespace GameClient.Common.Pages
     {
         private List<CheckerDto> _blackCheckers { get; set; } = new();
         private List<CheckerDto> _whiteCheckers { get; set; } = new();
-        private List<int> _rowPossible { get; set; } = new();
-        private List<int> _colomnsPossible { get; set; } = new ();
+        private List<(int row, int column)> _cellsPossible { get; set; } = new();
         private CheckerDto _activeChecker { get; set; }
         private bool _canMove { get; set; } = false;
 
@@ -42,7 +42,7 @@ namespace GameClient.Common.Pages
             {
                 for (int j = (i + 1) % 2; j < 8; j += 2)
                 {
-                    _blackCheckers.Add(new CheckerDto
+                    _whiteCheckers.Add(new CheckerDto
                     {
                         Color = "white",
                         Column = j,
@@ -55,25 +55,55 @@ namespace GameClient.Common.Pages
 
         private void EvaluateCheckerSpots()
         {
-            _rowPossible.Clear();
-            _colomnsPossible.Clear();
+            _cellsPossible.Clear();
 
             if (_activeChecker != null)
+            {
+                List<int> rowsPossible = new();
+
+                if(_activeChecker.Direction == CheckerDirection.Down || _activeChecker.Direction == CheckerDirection.Both)
                 {
-                _rowPossible.Add(_activeChecker.Row + (1 * (_activeChecker.Direction == CheckerDirection.Down ? 1 : -1)));
-                _colomnsPossible.Add(_activeChecker.Column - 1);
-                _colomnsPossible.Add(_activeChecker.Column + 1);
+                    rowsPossible.Add(_activeChecker.Row + 1);
+                }
+
+                if (_activeChecker.Direction == CheckerDirection.Up || _activeChecker.Direction == CheckerDirection.Both)
+                {
+                    rowsPossible.Add(_activeChecker.Row - 1);
+                }
+
+                foreach (var row in rowsPossible)
+                {
+                    EvaluateSpot(row, _activeChecker.Column - 1);
+                    EvaluateSpot(row, _activeChecker.Column + 1);
+                }
+            }
+        }
+
+        private void EvaluateSpot(int row, int column)
+        {
+            var blackChecker = _blackCheckers.FirstOrDefault(x => x.Row == row && x.Column == column);
+            var whiteChecker = _whiteCheckers.FirstOrDefault(x => x.Row == row && x.Column == column);
+            if(blackChecker == null && whiteChecker == null)
+            {
+                _cellsPossible.Add((row, column));
             }
         }
 
         private void MoveChecker(int row, int column)
         {
-            _canMove = _rowPossible.Contains(row) && _colomnsPossible.Contains(column);
+            _canMove = _cellsPossible.Contains((row, column));
             if (!_canMove)
                 return;
 
             _activeChecker.Column = column;
             _activeChecker.Row = row;
+
+            if (_activeChecker.Row == 0 && _activeChecker.Color == "white")
+                _activeChecker.Direction = CheckerDirection.Both;
+
+            if (_activeChecker.Row == 7 && _activeChecker.Color == "black")
+                _activeChecker.Direction = CheckerDirection.Both;
+
             _activeChecker = null;
             EvaluateCheckerSpots();
         }
