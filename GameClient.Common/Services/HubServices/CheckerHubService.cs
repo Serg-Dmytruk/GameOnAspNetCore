52a5e9528e2a2b496fb2f9feecabef5ca972a00e
Serhii <Sergsteamlol@gmail.com>
2021-10-16 15:29:12 +0300

    update

--- GameClient.Common/Services/HubServices/CheckerHubService.cs
+++ GameClient.Common/Services/HubServices/CheckerHubService.cs
@@ -4,15 +4,13 @@ using Microsoft.Extensions.Options;
 
 namespace GameClient.Common.Services.HubServices
 {
-    
+
     public class CheckerHubService
     {
-        private readonly IOptions<CheckersHubOption> _option;
-        HubConnection _hubConnection;
+        public HubConnection HubConnection { get; set; }
         public CheckerHubService(IOptions<CheckersHubOption> option)
         {
-            _option = option;
-            _hubConnection = new HubConnectionBuilder().WithUrl(option.Value.Connection).Build();
+            HubConnection = new HubConnectionBuilder().WithUrl(option.Value.Connection).Build();
         }
       
     }
---
+++ GameServer.Common/Services/TableService/TableService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace GameServer.Common.Services.TableService
+{
+   public  class TableService : ITableService
+    {
+        private Dictionary<string, int> _tables { get; set; } = new();
+
+        public async Task<IEnumerable<string>> GetAwailableTables()
+        {
+            return await Task.Run(() => _tables.Where(x => x.Value < 2).Select(x => x.Key).ToList());
+        }
+        public void AddTable(string tableId, int playerCount)
+        {
+            _tables.Add(tableId, playerCount);
+        }
+    }
+}
